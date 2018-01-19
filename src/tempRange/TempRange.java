daaf96adc8af4537a41346a57ac0e9733f024805
Jon Fincher <jfincher42@gmail.com>
2018-01-19 10:17:38 -0800

    Initial commit

---
+++ src/tempRange/TempRange.java
@@ -0,0 +1,24 @@
+package tempRange;
+
+public class TempRange {
+	// Add 2 private int field variables to
+	// track low and high temperatures here
+	public TempRange(int dayLow, int dayHigh) {
+		// Initialize the field variables here
+	}
+
+	public int getLowTemp() {
+		// return low temp
+	}
+	public int getHighTemp() {
+		// return high temp
+	}
+	public static double calcAverageLow(TempRange[] temps) {
+		// compute and return average of all
+		// low temps in the array
+	}
+	public static double calcAverageHigh(TempRange[] temps) {
+		// Compute and return the average of all
+		// high temps in the array
+	}
+}
